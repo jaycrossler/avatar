708bbe9596463c61cb4b0eecb045018036d6226f
jcrossler <jay.crossler@gmail.com>
2015-09-07 20:08:27 -0400

    Improve all race face generation example

--- examples/all_races.js
+++ examples/all_races.js
@@ -6,16 +6,16 @@ $(document).ready(function () {
     //For each race, modify the rendering instructions, and draw a template
     _.each(Avatar.getRaces(), function(race_name, i){
 
-        //Pull a pointer to the current avatar human template
+        //Pull a pointer to the current avatar  template for that race
         var AvatarRace = new Avatar('get_linked_template', race_name);
 
-        //Take out any existing decorations
+        //Remove any existing decorations
         AvatarRace.rendering_order = _.filter(AvatarRace.rendering_order, function(dec){return !dec.decoration});
 
-        //Add a new text decoration that shows the race
+        //Add a new text decoration that shows the race name in text under the face
         AvatarRace.rendering_order.push(
             {decoration: "race-plate", type: 'rectangle', height: 16, docked: 'bottom', forceInBounds: true, font_size: 9,
-             text: 'Race: {{race}}', text_color: 'black', line_color: 'brown', fill_color: 'white', alpha: 0.8}
+             text: '{{gender}} {{race}}, Age {{age}}', text_color: 'black', line_color: 'brown', fill_color: 'white', alpha: 0.8}
         );
 
         var $canvas = $("<canvas>")
@@ -25,5 +25,23 @@ $(document).ready(function () {
 
         var av = new Avatar({rand_seed: seed, race: race_name, age: 38}, {canvas_name: $canvas});
         avatars.push(av);
+
+        //On every click, generate a new random seed for each face
+        av.registerEvent('face', function (avatar) {
+             seed = Helpers.randInt(300000);
+
+             var age_of = avatar.face_options.age || "Avatar";
+             var text = " : new Avatar({rand_seed: " + seed + ", age:"+age_of+"});";
+             $('#avatar_name').text(text);
+
+             _.each(avatars,function(avatar_mini){
+                 var age_of = avatar_mini.face_options.age || "Avatar";
+                 var race = avatar_mini.face_options.race;
+                 avatar_mini.face_options = null;
+                 avatar_mini.drawOrRedraw({rand_seed: seed, age: age_of, race:race});
+             });
+
+         }, 'click');
+
     });
 });
