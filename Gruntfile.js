32d848ca1cf7f7fe4a44fae5fc710697dbc992a0
jcrossler <jay.crossler@gmail.com>
2015-09-30 16:57:12 -0400

    Grunt tasks to export to Dropbox

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,8 @@
 var libraryFiles = ['js-libs/color.js', 'js-libs/colors.min.js', 'js/maths.js', 'js/helpers.js'];
 var avatarFiles = [ 'js/avatar-<%= pkg.version %>.js', 'js/avatar-options.js', 'js/avatar-textures.js', 'js/avatar-lines.js', 'js/avatar-hair.js', 'js/avatar-beard.js'];
 var raceFiles = [ 'js/races/ogre.js', 'js/races/navi.js', 'js/races/demon.js'];
+var dropbox_root = '/Users/jcrossler/Dropbox/Public/sites/avatar/';
+
 
 var allFiles = libraryFiles.concat(avatarFiles, raceFiles);
 
@@ -91,6 +93,19 @@ module.exports = function (grunt) {
                 }
             }
         },
+        copy: {
+            dropbox: {
+                files: [
+                    {expand: true, src: ['build/**'], dest: dropbox_root+ 'build'},
+                    {expand: true, src: ['css/**'], dest: dropbox_root+ 'css'},
+                    {expand: true, src: ['examples/**'], dest: dropbox_root+ 'examples'},
+                    {expand: true, src: ['images/**'], dest: dropbox_root+ 'images'},
+                    {expand: true, src: ['js/**'], dest: dropbox_root+ 'js'},
+                    {expand: true, src: ['js-libs/**'], dest: dropbox_root+ 'js-libs'},
+                    {expand: false, src: ['index.html'], dest: dropbox_root}
+                ]
+            }
+        },
         replace: {
             version: {
                 src: ['js/avatar.js'],
@@ -105,6 +120,20 @@ module.exports = function (grunt) {
                         to: "summary = '<%= pkg.summary %>',"
                     }
                 ]
+            },
+            index: {
+                src: ['index.html'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: new RegExp('<.*?id="summary".*?>(.*?)</.*?>'),
+                        to: '<h2 id="summary"><%= pkg.summary %></h2>'
+                    },
+                    {
+                        from: new RegExp('<.*?id="description".*?>(.*?)</.*?>'),
+                        to: '<p id="description" class="lead"><%= pkg.description %></p>'
+                    }
+                ]
             }
         }
     });
@@ -117,11 +146,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
-    grunt.registerTask('default', ['replace:version', 'concat:build', 'uglify:build', 'jasmine', 'notify:build']);
+    grunt.registerTask('default', ['replace:index', 'replace:version', 'concat:build', 'uglify:build', 'jasmine', 'notify:build']);
     grunt.registerTask('quick', ['concat:quick', 'notify:quick']);
     grunt.registerTask('server', ['concat:quick', 'notify:quick', 'connect']);
+    grunt.registerTask('dropbox', ['copy:dropbox']);
 
     grunt.task.run('notify_hooks');
 
